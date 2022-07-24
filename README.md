694c095c790ce4a8f6ddb1b1dcf3cf8c2d815f5f
Ferro Mauro <ferromauro@yahoo.it>
2022-07-24 23:38:06 +0200

    Using Class Object

--- README.md
+++ README.md
@@ -3,29 +3,32 @@
 Gmail_reader is a Python script to read email from Gmail account.
 
 ## Settings
-In order to login to your gmail account you need to set some values.
-You have to create an 'app password', please follow this instructions: [Google Help App Password](https://support.google.com/accounts/answer/185833?hl=en)
-```python
-FROM_EMAIL  = [YOUR EMAIL]
-FROM_PWD    = [YOUR APP PASSWORD]
-SMTP_SERVER = "imap.gmail.com"
-SMTP_PORT   = 993
-```
+In order to login to your gmail account you need to set some values in the __init__().
 
+```python
+def __init__(self):    
+    self.gmail      = [YOUR EMAIL]
+    self.password   = [YOUR APP PASSWORD]
+    self.server     = "imap.gmail.com"
+    self.port       = 993
+```
+**Note:** You have to create an 'app password', please follow this instructions: [Google Help App Password](https://support.google.com/accounts/answer/185833?hl=en)
 
 ## Usage
 
 ```python
-import gmail_reader
+from gmail_reader import Gmail_reader
 
+# Create a reader instance
+G = Gmail_reader()
 # returns only last email without body
-read_email()
+G.read_()
 
 # returns last 5 email without body
-read_email(5)
+G.read(5)
 
 # returns last 5 emails with body
-read_email(5,True)
+G.read(5,True)
 ```
 
 
--- gmail_reader.py
+++ gmail_reader.py
@@ -1,65 +1,69 @@
 import imaplib
 import email
 
-FROM_EMAIL  = [YOUR EMAIL]
-FROM_PWD    = [YOUR APP PASSWORD]
-SMTP_SERVER = "imap.gmail.com"
-SMTP_PORT   = 993
+class Gmail_reader():
 
+    def __init__(self):    
+        self.gmail      = "[YOUR EMAIL]
+        self.password   = "[YOUR APP PASSWORD]
+        self.server     = "imap.gmail.com"
+        self.port       = 993
 
-def read_email(number=1,  contain_body=False):
-    try: 
-        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-        mail.login(FROM_EMAIL,FROM_PWD)
-        mail.select('inbox')
+    def read(self, number=1,  contain_body=False):
+        try: 
+            mail = imaplib.IMAP4_SSL(self.server)
+            mail.login(self.gmail, self.password)
+            mail.select('inbox')
 
-        data = mail.search(None, 'ALL')
-        mail_ids = data[1]
-        id_list = mail_ids[0].split()   
-        latest_email_id = int(id_list[-1])
+            data = mail.search(None, 'ALL')
+            mail_ids = data[1]
+            id_list = mail_ids[0].split()   
+            latest_email_id = int(id_list[-1])
 
-        for i in range(latest_email_id,latest_email_id-number, -1):
-            data = mail.fetch(str(i), '(RFC822)' )
+            for i in range(latest_email_id,latest_email_id-number, -1):
+                data = mail.fetch(str(i), '(RFC822)' )
 
-            for response_part in data:
-                arr = response_part[0]
-                if isinstance(arr, tuple):
-                    msg = email.message_from_string(str(arr[1],'utf-8'))
-                    # Subject
-                    subject = msg['subject']
-                    
-                    # Sender
-                    sender = msg['from']
-                
-                    # Store Body
-                    body = ""
-                    temp = msg
-                    if temp.is_multipart():
-                        for part in temp.walk():
-                            ctype = part.get_content_type()
-                            cdispo = str(part.get('Content-Disposition'))
+                for response_part in data:
+                    arr = response_part[0]
+                    if isinstance(arr, tuple):
+                        msg = email.message_from_string(str(arr[1],'utf-8'))
+                        # Subject
+                        subject = msg['subject']
 
-                            # skip text/plain type
-                            if ctype == 'text/plain' and 'attachment' not in cdispo:
-                                body = part.get_payload(decode=True)  # decode
-                                break
-                            else:
-                                body = temp.get_payload(decode=True)
-                    
-                    # Print Sender, Subject, Body
-                    print("-"*75)  # To divide the messages
-                    print(f"From    : {sender}")
-                    print(f"Subject : {subject}")
-                    if(contain_body):
-                        print(f"Body    : {body.decode()}")
-  
+                        # Sender
+                        sender = msg['from']
 
-    except Exception as e:
-        print(e)
+                        # Store Body
+                        body = ""
+                        temp = msg
+                        if temp.is_multipart():
+                            for part in temp.walk():
+                                ctype = part.get_content_type()
+                                cdispo = str(part.get('Content-Disposition'))
+
+                                # skip text/plain type
+                                if ctype == 'text/plain' and 'attachment' not in cdispo:
+                                    body = part.get_payload(decode=True)  # decode
+                                    break
+                                else:
+                                    body = temp.get_payload(decode=True)
+
+                        # Print Sender, Subject, Body
+                        print("-"*75)  # To divide the messages
+                        print(f"From    : {sender}")
+                        print(f"Subject : {subject}")
+                        if(contain_body):
+                            print(f"Body    : {body.decode()}")
     
-    finally:
-        mail.close()
-        mail.logout()
 
-# Let's test it!!!        
-read_email(5, True)
+        except Exception as e:
+            print(e)
+
+        finally:
+            mail.close()
+            mail.logout()
+
+# Let's test it!!! 
+if __name__ == "__main__": 
+    reader = Gmail_reader()      
+    reader.read(5, True)
